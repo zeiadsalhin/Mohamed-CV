a9419535e7b931d2e6643fa730b08bd42e1c3594
zeiadsalhin <zeyadali011@gmail.com>
2024-02-05 21:50:12 +0200

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+import info from './components/Info.vue'
 </script>
 
 <template>
-  <header>
-    <div class="center flex-col justify-center h-full w-full">
-      <img class="mx-auto px-10 m-5 my-auto" width="150" src="/circles.svg">
-      <h1 class="text-center text-5xl font-semibold">In progress</h1>
+  <header class="flex justify-around space-x-28">
+    <div class="logo p-5 flex"><a href="#"><img class="" width="150" src="/Mohamed.svg"></a></div>
+    <div class="nav p-5 space-x-10">
+      <button class="text-xl">About Me</button>
+      <button class="text-xl">Experience</button>
+      <button class="text-xl">Education</button>
+      <button class="text-xl">Contact</button>
     </div>
   </header>
-
+  <div class="line w-11/12 h-0.5 bg-gray-800 rounded-full mx-auto mt-2"></div>
   <main>
-    <TheWelcome />
+    <info />
   </main>
 </template>
 
@@ -24,20 +27,10 @@ header {
 
 @media (min-width: 1024px) {
   header {
-    color: var(--vt-c-text-dark-1);
+    color: var(--vt-c-text-dark-2);
     /* display: flex; */
     /* place-items: center; */
     /* padding-right: calc(var(--section-gap) / 2); */
   }
-
-
-
-}
-
-.center {
-  margin: auto;
-  width: 100%;
-  /* border: 3px solid green; */
-  padding: 20px;
 }
 </style>
---
+++ src/components/Info.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+<template>
+    <div class="main flex mt-10 p-5 w-full">
+        <div class="text w-1/2 my-auto">
+            <h1 class="text-6xl leading-tight   ">Hi, I am Mohamed Khaled, a Data Scientist</h1>
+            <button class="p-4 mt-10 text-xl bg-gray-800 l">Email me</button>
+        </div>
+        <div class="image w-1/2">
+            <img class="w-full mx-auto rounded-3xl" width="" src="/mohamed.jpg">
+        </div>
+    </div>
+</template>
+<style scoped>
+h1 {
+    color: var(--vt-c-text-dark-1);
+}
+
+.l {
+    color: var(--vt-c-text-dark-1);
+}
+
+.d {
+    color: var(--vt-c-text-dark-2);
+}
+</style>
